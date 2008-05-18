0241583f36d2b8f69830934c6e8986d7916be1a0
Dennis Moellegaard Pedersen <dennis@moellegaard.dk>
2008-05-18 10:23:52 +0200

    Unused variable

--- src/listserverhandler.cpp
+++ src/listserverhandler.cpp
@@ -209,7 +209,6 @@ Server* ListServerHandler::FindByName(const wxString& n) {
 // TODO: This will perform a search of the this->serverList. For now, it dosn't really
 // do anything, except return a ServerResultSet (like searching that matches everything)
 ServerResultSet ListServerHandler::Search(const Query& query) {
-	ServerList		serverList;
 	ServerResultSet	resultSet;
 
 	for(ServerList::iterator i = this->serverList.begin(); i != this->serverList.end(); ++i) {
