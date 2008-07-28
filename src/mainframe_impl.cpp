9125830566b8fecd7020f32edc185c7537274afd
Dennis Moellegaard Pedersen <dennis@moellegaard.dk>
2008-07-28 21:37:20 +0200

    Reduce the verbosity of the debug-output

--- src/mainframe_impl.cpp
+++ src/mainframe_impl.cpp
@@ -298,6 +298,7 @@ void MainFrameImpl::RefreshActiveView() {
 	std::vector<Label*>	columns = this->GetViewableLabels();
 
 	QueryResult res = app.dataControl.search(this->activeView->query);
+	wxLogDebug(_T("Query: '%s' found %d matches"), this->activeView->query.get().c_str(), res.size());
 
 	// For now, always re-created the list
 	list->DeleteAllItems();
@@ -306,8 +307,6 @@ void MainFrameImpl::RefreshActiveView() {
 		int idx = 0;
 		for(std::vector<Label*>::iterator i  = columns.begin(); i != columns.end() ; ++i ) {
 			const wxString val = res.getAttribute(row, *i);
-			wxLogDebug(_T("Updating col %d label %s, row %d, %s"),
-				col, (*i)->getName().c_str(), row, val.c_str());
 			if( col == 0 )
 				idx = list->InsertItem(col, val);
 			else if(idx != -1)
